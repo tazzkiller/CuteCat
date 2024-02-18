ac0f9122d28f6183a34420068b381524165e38db
Tazzkiller <tazzkiller@hotmail.com>
2024-02-18 20:05:45 +0400

    switch to Napi canvas for image & text

--- commands/cutcat/cutecat.js
+++ commands/cutcat/cutecat.js
@@ -1,4 +1,18 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, AttachmentBuilder } = require("discord.js");
+const { createCanvas, loadImage, GlobalFonts } = require("@napi-rs/canvas");
+const { join } = require('path');
+const fontPath = join(__dirname, '../..', 'fonts', 'COMIC.ttf');
+GlobalFonts.registerFromPath(fontPath);
+
+const applyText = (canvas, text) => {
+  const context = canvas.getContext("2d");
+  let fontSize = 300;
+
+  do {
+    context.font = `${(fontSize -= 4)}px 'Comic Sans MS'`;
+  } while (context.measureText(text).width > canvas.width - 50);
+  return context.font;
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,18 +23,27 @@ module.exports = {
     ),
   async execute(interaction) {
     const message = interaction.options.getString("message") ?? " ";
-    const thecat =
-      "https://cataas.com/cat/says/" +
-      encodeURIComponent(message.toUpperCase()) +
-      "?font=Impact&fontSize=30&fontColor=%23fff&fit=contain&position=bottom";
-    await interaction.reply({
-      content: " ",
-      files: [
-        {
-          attachment: thecat,
-          name: "cutecat.png",
-        },
-      ],
+    //const messageUpperText = message.toUpperCase();
+    const messageUpperText = message;
+    const thecaturl = "https://cataas.com/cat";
+    const thecat = await loadImage(thecaturl);
+    const canvas = createCanvas(thecat.width, thecat.height);
+    const context = canvas.getContext("2d");
+    context.drawImage(thecat, 0, 0, canvas.width, canvas.height);
+    context.textBaseline = "middle";
+    context.font = applyText(canvas, messageUpperText);
+    const textWidth = context.measureText(messageUpperText).width;
+    context.shadowColor = "black";
+    context.shadowBlur = 8;
+    context.fillStyle = "#ffffff";
+    context.fillText(
+      messageUpperText,
+      canvas.width / 2 - textWidth / 2,
+      canvas.height - 100
+    );
+    const attachment = new AttachmentBuilder(await canvas.encode("png"), {
+      name: "cutecat.png",
     });
+    await interaction.reply({ files: [attachment] });
   },
 };
